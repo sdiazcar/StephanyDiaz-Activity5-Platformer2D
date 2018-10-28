0222f228fb741ae68beb39b4e4be1fe7b295561c
Stephany Diaz Cartagena <asocoba96@hotmail.com>
2018-10-27 20:12:38 -0500

    1

---
+++ StephanyDiaz-Platformer2D/Assets/Scripts/ButtonJumpAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonJumpAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void TapEnElBotonJump(){
+        Debug.Log("El Boton Jump Ha Sido Presionado");
+
+        GameObject.Find("Mario").GetComponent<MarioCharacterController>().EjecuteElSaltoDeMario();
+    }
+}
